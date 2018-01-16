f38742798b0a400763e32915ff47d347f7f88cef
Julian Kaping <kaping@govconnect.de>
2018-01-16 13:23:46 +0100

    Improved performance of starfield

--- 01_Starfield/sketch.js
+++ 01_Starfield/sketch.js
@@ -7,21 +7,41 @@
 
 let stars = [];
 let speed = 25;
+let starCount;
 
 function setup() {
-  createCanvas(800, 800);
+  let canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.position(0, 0);
+
+  if (window.innerWidth >= window.innerHeight) {
+    starcount = width / 3;
+  } else {
+    starcount = height / 3;
+  }
+
   for (let i = 0; i < 300; i++)
     stars.push(new Star());
 }
 
 function draw() {
+  push();
   background(0);
   translate(width / 2, height / 2);
-  speed = map(mouseX, 0, width, 0, 25);
+  speed = map(mouseX, 0, width, 0, 75);
   // translate(mouseX, mouseY);
 
+  fill(255);
+  noStroke();
   for (let i = 0; i < stars.length; i++) {
     stars[i].update();
-    stars[i].show();
+    if (stars[i].circle) {
+      stars[i].showCircle();
+    }
   }
+
+  stroke(255);
+  for (let i = 0; i < stars.length; i++)
+    stars[i].showLine();
+
+  pop();
 }
--- 01_Starfield/star.js
+++ 01_Starfield/star.js
@@ -1,29 +1,48 @@
 class Star {
   constructor() {
-    this.x = random(-(width / 2), (width / 2));
-    this.y = random(-(height / 2), (height / 2));
-    this.z = random(0, width);
+    this.x;
+    this.y;
+    this.z;
+    this.previous_z;
+    this.renew();
+    this.circle = false;
   }
 
   renew() {
-    this.x = random(-(width / 2), (width / 2));
-    this.y = random(-(height / 2), (height / 2));
-    this.z = width;
+    this.x = random(-width, width);
+    this.y = random(-height, height);
+    this.z = random(width);
   }
 
   update() {
-    this.z = this.z - speed;
-    if (this.z < 1)
+    this.z -= speed;
+    if (this.z < 1) {
       this.renew();
+      this.previous_z = this.z;
+    }
+
+    if (speed < 20) {
+      this.circle = true;
+    } else if (speed > 20) {
+      this.circle = false;
+    }
   }
 
-  show() {
+  showCircle() {
     let sx = map(this.x / this.z, 0, 1, 0, width);
     let sy = map(this.y / this.z, 0, 1, 0, height);
-    let r = map(this.z, 0, width, 16, 0);
-
-    fill(255);
-    noStroke();
+    let r = map(this.z, 0, width, 6, 0);
     ellipse(sx, sy, r);
+  }
+
+  showLine() {
+    let sx = map(this.x / this.z, 0, 1, 0, width);
+    let sy = map(this.y / this.z, 0, 1, 0, height);
+    let px = map(this.x / this.previous_z, 0, 1, 0, width);
+    let py = map(this.y / this.previous_z, 0, 1, 0, height);
+
+    line(px, py, sx, sy);
+
+    this.previous_z = this.z;
   }
 }
